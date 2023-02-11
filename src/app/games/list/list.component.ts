5faa6bf618e6871f13fa474779dca203dec8e6d1
Jackseed <vinkho.c@gmail.com>
2023-02-11 11:30:43 +0100

    game list

--- src/app/games/list/list.component.ts
+++ src/app/games/list/list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Game, GameQuery } from '../_state';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
-
-  constructor() { }
+  public gameList$: Observable<Game[]>;
+  constructor(private query: GameQuery) {}
 
   ngOnInit(): void {
+    this.gameList$ = this.query.selectAll();
+    this.gameList$.subscribe(console.log);
   }
-
 }
